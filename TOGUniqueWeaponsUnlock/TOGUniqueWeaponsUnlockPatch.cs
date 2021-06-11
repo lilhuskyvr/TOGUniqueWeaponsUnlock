d0aaeb4931d453c2cce0b6bafbca329d9d96025d
Lil Husky VR <lilhuskyvr@gmail.com>
2021-06-11 23:01:33 +1000

    version 1.0

---
+++ TOGUniqueWeaponsUnlock/TOGUniqueWeaponsUnlockPatch.cs
@@ -0,0 +1,21 @@
+﻿using TOGModFramework;
+using UnityEngine;
+
+namespace TOGUniqueWeaponsUnlock
+{
+    public class TOGUniqueWeaponsUnlockPatch : MonoBehaviour
+    {
+        public void Inject()
+        {
+            EventManager.OnPlayerLoaded += EventManagerOnOnPlayerLoaded;
+        }
+
+        private void EventManagerOnOnPlayerLoaded()
+        {
+            for (int i = 0; i < ConfigManager.local.spw.Length; i++)
+            {
+                ConfigManager.local.spw[i] = 1;
+            }
+        }
+    }
+}
